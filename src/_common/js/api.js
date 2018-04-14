0028a6ce836264757738d2dd5b86500a6870ad38
ziwen fan <zfan40@gatech.edu>
2018-04-14 21:31:18 +0800

    change api back to header auth

--- src/_common/js/api.js
+++ src/_common/js/api.js
@@ -8,7 +8,7 @@ const reqConfig = {
     'Content-Type': 'application/json',
   },
 };
-const tokenObj = { access_token: '' };
+// const tokenObj = { access_token: '' };
 // TODO:cause 500?????? just cuz this few lines???
 
 
@@ -16,9 +16,10 @@ export function getUserInfo(wxcode) {
   const tokenInCookie = Cookies.get('serviceToken');
   if (tokenInCookie) {
     // alert('cookie used');
-    // reqConfig.headers.Authorization = `Bearer ${tokenInCookie}`;
-    tokenObj.access_token = tokenInCookie;
-    return axios.get('//api.musixise.com/api/v1/user/getInfo', { params: tokenObj }, reqConfig);
+    reqConfig.headers.Authorization = `${tokenInCookie}`;
+    // alert(tokenInCookie);
+    // tokenObj.access_token = tokenInCookie;
+    return axios.get('//api.musixise.com/api/v1/user/getInfo', '', reqConfig);
   } else if (!tokenInCookie && wxcode) {
     // alert('no cookie');
     return new Promise((resolve, reject) => {
@@ -28,12 +29,12 @@ export function getUserInfo(wxcode) {
             reject(20001); // 相当于return了
           }
           const serviceToken = res.data.data.id_token;
-          // reqConfig.headers.Authorization = `Bearer ${serviceToken}`;
-          tokenObj.access_token = serviceToken;
+          reqConfig.headers.Authorization = `${serviceToken}`;
+          // tokenObj.access_token = serviceToken;
           Cookies.set('serviceToken', serviceToken, {
             expires: 7,
           });
-          return axios.get('//api.musixise.com/api/v1/user/getInfo', { params: tokenObj }, reqConfig);
+          return axios.get('//api.musixise.com/api/v1/user/getInfo', '', reqConfig);
         }, () => {
           reject();
         })
@@ -51,28 +52,29 @@ export function getUserInfo(wxcode) {
 
 export function uploadRecord(record, info) {
   return new Promise((resolve, reject) => {
-    const formReqConfig = { headers: { 'Content-Type': 'multipart/form-data', processData: false } };
+    const formReqConfig = { headers: { 'Content-Type': 'multipart/form-data', processData: false, Authorization: reqConfig.headers.Authorization } };
     const blob = new Blob([JSON.stringify(record)]);
     const reader = new FileReader();
     reader.onload = async (event) => {
       const fd = new FormData();
       fd.append('fname', 'test.txt');
       fd.append('data', event.target.result);
+      // fd.append('access_token', tokenObj.access_token);
       const postFix = await axios.post('//api.musixise.com/api/v1/uploadAudio', fd, formReqConfig);
       const workURL = `http://oiqvdjk3s.bkt.clouddn.com/${postFix.data.data}`;
       const param = {
-        ...tokenObj,
+        // ...tokenObj,
         ...info,
         url: workURL,
       };
       console.log(workURL);
       return axios.post('//api.musixise.com/api/v1/work/create', JSON.stringify(param), reqConfig)
-               .then((res) => {
-                 resolve(res.data.data.id);
-               })
-               .catch((err) => {
-                 reject(err);
-               });
+         .then((res) => {
+           resolve(res.data.data.id);
+         })
+         .catch((err) => {
+           reject(err);
+         });
     };
     reader.readAsDataURL(blob);
   });
@@ -80,24 +82,24 @@ export function uploadRecord(record, info) {
 }
 
 export function fetchMbox(id) {
-  return axios.get(`//api.musixise.com/api/v1/work/detail/${id}`, { params: tokenObj }, reqConfig);
+  return axios.get(`//api.musixise.com/api/v1/work/detail/${id}`, '', reqConfig);
 }
 export function fetchMusixiser(id) {
-  return axios.get(`//api.musixise.com/api/v1/user/detail/${id}`, { params: tokenObj }, reqConfig);
+  return axios.get(`//api.musixise.com/api/v1/user/detail/${id}`, '', reqConfig);
 }
 export function fetchWorksFromMusixiser(id, page) {
-  return axios.get(`//api.musixise.com/api/v1/work/getListByUid/${id}`, { params: { ...tokenObj, page, size: 15 } }, reqConfig);
+  return axios.get(`//api.musixise.com/api/v1/work/getListByUid/${id}`, { params: { page, size: 15 } }, reqConfig);
 }
 export function toggleFavSong({ workId, status }) {
-  return axios.post('//api.musixise.com/api/v1/favorite/addWork', JSON.stringify({ workId, status, ...tokenObj }), reqConfig)
+  return axios.post('//api.musixise.com/api/v1/favorite/addWork', JSON.stringify({ workId, status }), reqConfig)
     .then((res) => { console.log(res); })
     .catch((err) => { console.log(err); });
 }
 export function updateWorkTitle({ workId, title }) {
-  return axios.put(`//api.musixise.com/api/v1/work/updateWork/${workId}`, JSON.stringify({ workId, title, ...tokenObj }), reqConfig)
+  return axios.put(`//api.musixise.com/api/v1/work/updateWork/${workId}`, JSON.stringify({ workId, title }), reqConfig)
     .then((res) => { console.log(res); })
     .catch((err) => { console.log(err); });
 }
 export function getRecommendations() {
-  return axios.post('//api.musixise.com/api/v1/home', JSON.stringify(tokenObj), reqConfig);
+  return axios.post('//api.musixise.com/api/v1/home', '', reqConfig);
 }
--- src/_common/js/magic.js
+++ src/_common/js/magic.js
@@ -243,3 +243,64 @@ export function preview(items) {
   //   Tone.Transport.start('+0.01', 0);
   // }
 }
+
+// back up version 1 maker script
+// export function generateFreqs(notesInfo) {
+//   notesObj = {};
+//   notesArray = [];
+//   FreqsResult = [];
+//   console.log(notesInfo);
+//   notesInfo.forEach((item) => {
+//     // console.log(item.note)
+//     // console.log(notesObj)
+//     if (!(item.note in notesObj)) {
+//       console.log('!!');
+//       notesObj[item.note] = 1;
+//       notesArray.push({
+//         note: item.note,
+//         freq: Tone.Frequency(item.note).toFrequency(),
+//       });
+//     }
+//   });
+//   notesArray.sort((a, b) => (a.freq - b.freq));
+//   console.log(`本首作品共有${notesArray.length}种音符`);
+//   notesArray.forEach((item, index) => {
+//     FreqsResult.push(item.freq * 2);
+//     notesObj[item.note] = index + 1;
+//   });
+//   console.log('它们的频率是', FreqsResult);
+//   return FreqsResult;
+// }
+//
+// export function generateJSCadCode(notesInfo) {
+//   let musicboxPins = [];
+//   // norm timing to 15 second
+//   const lastNoteTime = notesInfo[notesInfo.length - 1].time;
+//   if (lastNoteTime > 15) {
+//     notesInfo.forEach((item) => {
+//       item.time = (item.time * 15) / lastNoteTime;
+//     });
+//   }
+//
+//   generateFreqs(notesInfo);
+//   musicboxPins = notesInfo.map(item => `generatePin(${item.time},${notesObj[item.note]})`);
+//   console.log(`
+//   const DOT_WIDTH = 0.6
+//   const RATIO = 0.98
+//   const OFFSET = 2.2 //1.95 is center
+//   const OUTER_RADIUS = 6.6
+//   const INNER_RADIUS = 5.9
+//   function generatePin(noteSec, noteNo) {
+//     return rotate(90, [1, 0, 4 * noteSec * RATIO / 15], cylinder({
+//       h: 1,
+//       r: DOT_WIDTH / 2,
+//       center: true
+//     })).translate([sin(360 * noteSec * RATIO / 15) * OUTER_RADIUS, -cos(360 * noteSec * RATIO / 15) * OUTER_RADIUS, -9.95 + OFFSET + 0.4 + (noteNo - 1) * .9])
+//   }
+//
+//   function main() {
+//     let cylinderBody = difference(cylinder({h: 19.9,r: OUTER_RADIUS,center: true}),cylinder({h: 19.9,r: INNER_RADIUS,center: true}))
+//     let holes = union(${musicboxPins})
+//     return union(cylinderBody,holes).translate([0, 0, 0]).scale(1);
+//   }`);
+// }
